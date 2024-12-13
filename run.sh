d66731dad788764c5720b1efdbf0df1ea344c542
PavaniVemasani18 <vemasani.pavani@gmail.com>
2024-12-14 00:27:35 +0530

    * get vault secrets from vault * inject variables to initcontainer * share data from init container to container

---
+++ run.sh
@@ -0,0 +1,21 @@
+if [ -z "$DB_HOST" ]; then
+ echo DB-HOST is missing
+ exit 1
+fi
+if [ -z "$DB_USER" ]; then
+ echo DB_USER is missing
+ exit 1
+fi
+if [ -z "$DB_PASS" ]; then
+ echo DB_PASS is missing
+ exit 1
+fi
+if [ -z "$component" ]; then
+ echo component is missing
+fi
+if [ -z "$project_name" ]; then
+ echo project name is missing
+fi
+git clone https://github.com/pdevpos/${project_name}-${component}.git
+cd ${project_name}-${component}
+mysql -h ${DB_HOST} -u${DB_USER} -p${DB_PASS} < /app/schema/${component}.sql
